8df113796dcd92a356dbab701019ac8ddafc939c
ivan domnikov <ivan@haleytek.com>
2023-12-01 23:57:32 +0100

    TEST RUN: fre  1 dec 2023 23:57:32 CET

--- 01/main.cpp
+++ 01/main.cpp
@@ -19,6 +19,7 @@ void ReplaceAll(std::string& str, const std::string& from, const std::string& to
 
 auto transform(S str) {
     auto copy = str;
+    ReplaceAll(str, "twone", "2no");
     ReplaceAll(str, "one", "1");
     ReplaceAll(str, "eightwo", "8wo");
     ReplaceAll(str, "eighthree", "8hree");
