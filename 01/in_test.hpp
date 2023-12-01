18288cbdf0564396ea02876925801f0b1e6cc151
ivan domnikov <ivan@haleytek.com>
2023-12-01 18:01:56 +0100

    TEST RUN: fre  1 dec 2023 18:01:56 CET

--- 01/in_test.hpp
+++ 01/in_test.hpp
@@ -4,13 +4,13 @@
 inline auto getInput()
 {
     VECS input {
-"two1nine";
-"eightwothree";
-"abcone2threexyz";
-"xtwone3four";
-"4nineeightseven2";
-"zoneight234";
-"7pqrstsixteen";
+"two1nine",
+"eightwothree",
+"abcone2threexyz",
+"xtwone3four",
+"4nineeightseven2",
+"zoneight234",
+"7pqrstsixteen",
     };
 
     return input;
