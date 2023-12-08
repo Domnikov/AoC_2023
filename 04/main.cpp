4ef991c2a3ffa8e47e9094fe1605de4453855564
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-08 02:50:38 +0100

    Add days: 2, 3, 4, 5, 6, 7

---
+++ 04/main.cpp
@@ -0,0 +1,27 @@
+#ifdef TEST
+#include "in_test.hpp"
+#else
+#include "in.hpp"
+#endif
+
+#include <queue>
+#include <list>
+
+auto in = getInput();
+
+auto count() {
+    LL result = 0;
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    LL score = 0;
+    score = count();
+    P_RR("Part1: %lld\n", score);
+    //========================================================
+
+    score = count();
+    P_RR("Part2: %lld\n", score);
+    return 0;
+}
