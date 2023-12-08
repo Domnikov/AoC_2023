634127e38d1ab54a6dbf1f37a926d273fd3dd617
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-08 07:56:09 +0100

    TEST RUN: Fri Dec  8 07:56:09 AM CET 2023

--- 03/main.cpp
+++ 03/main.cpp
@@ -6,11 +6,64 @@
 
 #include <queue>
 #include <list>
+#include <set>
 
 auto in = getInput();
 
+char get(LL x, LL y) {
+    return in[y][x];
+}
+
+bool is_numer(LL x, LL y){
+    if(x < 0 || y < 0 || y >= in.size() || x >= in[y].size()) {
+        return false;
+    }
+    auto c = get(x,y);
+    return c >= 0x30 && c <= 0x39;
+
+}
+
+bool if_simbol(LL x, LL y){
+    if(x < 0 || y < 0 || y >= in.size() || x >= in[y].size()) {
+        return false;
+    }
+    auto c = get(x,y);
+    return !is_numer(x,y) || c == '.';
+
+}
+
+bool is_adjacent(LL x, LL y) {
+    return if_simbol(x - 1, y - 1) ||
+           if_simbol(x - 0, y - 1) ||
+           if_simbol(x + 1, y - 1) ||
+           if_simbol(x - 1, y - 0) ||
+           if_simbol(x + 1, y - 0) ||
+           if_simbol(x - 1, y + 1) ||
+           if_simbol(x - 0, y + 1) ||
+           if_simbol(x + 1, y + 1);
+}
+
 auto count() {
     LL result = 0;
+    VECI not_adj;
+    LL nan = -666;
+    FOR(y, in.size()){
+        LL num = nan;
+        bool is_adj = true;
+        FOR(x, in[y].size()){
+            if(is_numer(x, y)){
+                num = 10*num+(in[y][x]-0x30);
+            }
+            else if(num != nan){
+                if(!is_adj){
+                    not_adj.push_back(num);
+                }
+                num = nan;
+                is_adj=true;
+            }
+        }
+    }
+    P_VEC(not_adj);
     return result;
 }
 
