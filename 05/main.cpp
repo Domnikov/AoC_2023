bce00da98e69005d4b4ffa599678853717375dbe
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-12 06:49:11 +0100

    TEST RUN: Tue Dec 12 06:49:11 AM CET 2023

--- 05/main.cpp
+++ 05/main.cpp
@@ -9,8 +9,6 @@
 
 auto in = getInput();
 
-VECII seeds;
-
 VECI GetInitialSeeds() {
     auto vecS = splitStr(in[0], ' ');
 
@@ -34,37 +32,27 @@ size_t FindNext(size_t idx) {
 }
 
 auto count() {
-    seeds.push_back(GetInitialSeeds());
+    auto seeds = GetInitialSeeds();
     size_t idx = 1;
     while((idx = FindNext(idx)) != -1) {
         P(in[idx]);
-        std::map<LL, LL> map;
         while(!in[++idx].empty()){
             P(in[idx]);
             auto vec = vecsToVeci(splitStr(in[idx], ' '));
             LL src = vec[1];
             LL dst = vec[0];
             LL sz = vec[2];
-            for(LL i = 0; i < sz; ++i) {
-                map[src+i] = dst+i;
+            for(auto& s:seeds) {
+                if(s >= src && s <= src+sz){
+                    s = dst + src - s;
+                }
             }
         }
-        VECI new_seeds;
-        VECI old_seeds = seeds[seeds.size()-1];
-        for(auto s:old_seeds) {
-            if(map.count(s)){
-                new_seeds.push_back(map[s]);
-            } else {
-                new_seeds.push_back(s);
-            }
-        }
-        seeds.push_back(new_seeds);
-        // P_VEC(seeds);
+        P_VEC(seeds);
     }
-    auto locations = seeds[seeds.size()-1];
     // P_VEC(locations);
-    LL min = locations[0];
-    for(auto n : locations){
+    LL min = seeds[0];
+    for(auto n : seeds){
         if(min > n) {
             min = n;
         }
