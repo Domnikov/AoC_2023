65cac619c54a6c6b96b2339271d89a1fc307900f
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-12 07:16:33 +0100

    TEST RUN: Tue Dec 12 07:16:33 AM CET 2023

--- 06/main.cpp
+++ 06/main.cpp
@@ -9,12 +9,19 @@
 
 auto in = getInput();
 
+LL play (LL t, LL d) {
+    return 10;
+}
+
 auto count() {
     LL result = 0;
     auto vecSTime = splitStr(in[0], ' ');
     auto vecSDist = splitStr(in[1], ' ');
     P_VEC(vecSTime);
     P_VEC(vecSDist);
+    for(size_t i = 1; i < vecSTime.size(); ++i) {
+        result += play(stoll(vecSTime[i]), stoll(vecSDist[i]));
+    }
     return result;
 }
 
