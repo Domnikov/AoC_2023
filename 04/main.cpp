18faf6f4b3d537d2d6c4bf1775696c897c0a2e03
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-12 03:17:59 +0100

    TEST RUN: Tue Dec 12 03:17:59 AM CET 2023

--- 04/main.cpp
+++ 04/main.cpp
@@ -47,6 +47,21 @@ auto count() {
     return result;
 }
 
+auto count_2() {
+    LL result = 0;
+    for(auto s : in){
+        auto card = stoi(splitStr(splitStr(s,':')[0], ' ')[1]);
+        auto swins = splitStr(splitStr(splitStr(s, ':')[1], '|')[0], ' ');
+        auto smy = splitStr(splitStr(splitStr(s, ':')[1], '|')[1], ' ');
+        VECI wins, my;
+        VECSTOA(swins, wins);
+        VECSTOA(smy, my);
+        // P(getScore(getHaveWins(wins, my).size()));
+        result += getScore(getHaveWins(wins, my).size());
+    }
+    return result;
+}
+
 int main(int argc, char** argv)
 {
     LL score = 0;
@@ -54,7 +69,7 @@ int main(int argc, char** argv)
     P_RR("Part1: %lld\n", score);
     //========================================================
 return 0;
-    score = count();
+    score = count_2();
     P_RR("Part2: %lld\n", score);
     return 0;
 }
