e6b7cdbfa349403239bbf9636bcc033b83809a62
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-13 08:02:08 +0100

    TEST RUN: Wed Dec 13 08:02:08 AM CET 2023

--- 08/in_test.hpp
+++ 08/in_test.hpp
@@ -4,6 +4,15 @@
 inline auto getInput()
 {
     VECS input {
+"RL",
+"",
+"AAA = (BBB, CCC)",
+"BBB = (DDD, EEE)",
+"CCC = (ZZZ, GGG)",
+"DDD = (DDD, DDD)",
+"EEE = (EEE, EEE)",
+"GGG = (GGG, GGG)",
+"ZZZ = (ZZZ, ZZZ)",
     };
 
     return input;
--- 08/main.cpp
+++ 08/main.cpp
@@ -9,6 +9,16 @@
 
 auto in = getInput();
 
+std::map<S, std::pair<S,S>> M;
+
+LL cur = 0;
+
+bool GetNext() {
+    LL step = cur%in.size();
+    ++cur;
+    return in[0][step] == 'R';
+}
+
 auto count() {
     LL result = 0;
     return result;
@@ -16,6 +26,15 @@ auto count() {
 
 int main(int argc, char** argv)
 {
+    for(size_t i = 2; i < in.size();++i){
+        auto v1 = splitStr(in[i], '=');
+        auto v2 = splitStr(v1[1],',');
+        S left(v2[0].begin()+1, v2[0].end());
+        v2[1].pop_back();
+        S right(v2[1].begin(), v2[1].end());
+        M[v1[0]] = std::make_pair(left, right);
+    }
+
     LL score = 0;
     score = count();
     P_RR("Part1: %lld\n", score);
