2e35426bf4309c4c49f734a9a4ae8d48d459689d
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-13 08:08:58 +0100

    TEST RUN: Wed Dec 13 08:08:58 AM CET 2023

--- 08/main.cpp
+++ 08/main.cpp
@@ -24,7 +24,10 @@ auto count() {
     S cur = "AAA";
     while(cur != "ZZZ") {
         ++result;
+        P(cur);
         cur = GetNext() ? M[cur].first : M[cur].second;
+        P(cur, M[cur].first, M[cur].second);
+        return 0LL;
     }
     return result;
 }
