93a70ab8a21999908598cd10fb81cfcf5af86e54
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-13 02:11:14 +0100

    TEST RUN: Wed Dec 13 02:11:14 AM CET 2023

--- 05/main.cpp
+++ 05/main.cpp
@@ -71,8 +71,8 @@ std::pair<VECPLL, VECPLL> GetCross(std::pair<LL, LL>& seed, LL src, LL dst, LL s
 auto count(VECPLL seeds) {
     size_t idx = 1;
     while((idx = FindNext(idx)) != -1) {
-        P_RR("\n");
-        P(in[idx]);
+        // P_RR("\n");
+        // P(in[idx]);
         VECPLL converted;
         while(!in[++idx].empty()){
             auto vec = vecsToVeci(splitStr(in[idx], ' '));
