b935546b6967355aa9c4c57270bf2640dc973cfc
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-13 08:22:23 +0100

    TEST RUN: Wed Dec 13 08:22:23 AM CET 2023

--- 08/in_test.hpp
+++ 08/in_test.hpp
@@ -4,14 +4,10 @@
 inline auto getInput()
 {
     VECS input {
-"RL",
+"LLR",
 "",
-"AAA = (BBB, CCC)",
-"BBB = (DDD, EEE)",
-"CCC = (ZZZ, GGG)",
-"DDD = (DDD, DDD)",
-"EEE = (EEE, EEE)",
-"GGG = (GGG, GGG)",
+"AAA = (BBB, BBB)",
+"BBB = (AAA, ZZZ)",
 "ZZZ = (ZZZ, ZZZ)",
     };
 
