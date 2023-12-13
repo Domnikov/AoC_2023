f147cec8545f9c2f168fcf5126cba394874640fd
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-13 15:09:13 +0100

    TEST RUN: Wed Dec 13 03:09:13 PM CET 2023

--- 08/main.cpp
+++ 08/main.cpp
@@ -53,7 +53,7 @@ LL count2() {
                 cur = nextS;
                 // P(cur[2], finds.count({cur, n%N}));
             }
-            return result;
+            P(result);
         }
     }
     return result;
