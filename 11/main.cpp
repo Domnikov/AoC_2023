19f9c499c09b64fa83047489456a7f97f68391cc
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-14 05:16:23 +0100

    REAL RUN: Thu Dec 14 05:16:23 AM CET 2023

--- 11/main.cpp
+++ 11/main.cpp
@@ -9,6 +9,12 @@
 
 auto in = getInput();
 
+void expand(){
+    for(size_t i = in.size() - 1; i >= 0; --i){
+
+    }
+}
+
 auto count() {
     LL result = 0;
     return result;
@@ -17,6 +23,7 @@ auto count() {
 int main(int argc, char** argv)
 {
     LL score = 0;
+    P_VECV(in);
     score = count();
     P_RR("Part1: %lld\n", score);
     //========================================================
