0c82e9506c23d3051a41b6ec60c9799cbd3f5733
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-14 12:07:46 +0100

    TEST RUN: Thu Dec 14 12:07:46 PM CET 2023

--- 12/main.cpp
+++ 12/main.cpp
@@ -152,6 +152,7 @@ LL recursion(const S& s, LL posS, const VECI& nums, LL posN){
 auto count1() {
     LL result = 0;
     FOR(i, in.size()){
+        P(i);
         auto nums = GetNumb(i);
         const S& s = GetLine(i);
         LL unk = CountUnknowns(s);
