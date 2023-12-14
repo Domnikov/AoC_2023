e1b20c7d2f1ac7a3389469f1221600631c2b1578
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-14 05:29:35 +0100

    TEST RUN: Thu Dec 14 05:29:35 AM CET 2023

--- 11/main.cpp
+++ 11/main.cpp
@@ -9,8 +9,35 @@
 
 auto in = getInput();
 
+bool is_emptyRow(LL l){
+    FOR(i, in[l].size()){
+        if(in[l][i] != '.') return false;
+    }
+    return true;
+}
+
+bool is_emptyCol(LL c){
+    FOR(i, in.size()){
+        if(in[i][c] != '.') return false;
+    }
+    return true;
+}
+
+void insertRow(LL l){
+    in.insert(in.begin() + l+1, in[l]);
+}
+
+void insertCol(LL c){
+    FOR(i, in.size()){
+        in[c].insert(in[c].begin() + c+1, '.');
+    }
+}
+
 void expand(){
     for(LL i = in.size() - 1; i >= 0; --i){
+        if(is_emptyRow(i)){
+            insertRow(i);
+        }
 
     }
 }
