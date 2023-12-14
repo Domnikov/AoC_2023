f5383e830ed446e5f2557c43f7442e95a7635347
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-14 08:32:36 +0100

    TEST RUN: Thu Dec 14 08:32:36 AM CET 2023

--- 12/main.cpp
+++ 12/main.cpp
@@ -9,8 +9,65 @@
 
 auto in = getInput();
 
+S GetLine(LL i){
+    return splitStr(in[i], ' ')[0];
+}
+
+VECI GetNumb(LL i){
+    return vecsToVeci(splitStr(splitStr(in[i], ' ')[0], ','));
+}
+
+VECI StrToBrokenVeci(S s){
+    VECI result;
+    result.push_back(0);
+    for(auto c:s){
+        if(c == '#'){
+            result.back()++;
+        }
+        else if(result.back()){
+            result.push_back(0);
+        }
+    }
+    return result;
+}
+
+bool IsCorrect(S s, VECI nums) {
+    return StrToBrokenVeci(s) == nums;
+}
+
+LL CountUnknowns(S s){
+    LL counter = 0;
+    for(auto c:s){
+        if(c == '?') ++counter;
+    }
+    return counter;
+}
+
+LL permutation = 0;
+
+void resetPerm(){
+    permutation = 0;
+}
+
+bool Next(LL len){
+    return (pow(2, len) < ++permutation);
+}
+
+S GetPermutation(LL len){
+    LL cur = permutation;
+    S result;
+    FOR(i, len){
+        result.push_back((cur%2) ? '#' : '.');
+        cur /= 2;
+    }
+    return result;
+}
+
 auto count() {
     LL result = 0;
+    do{
+        P(GetPermutation(4));
+    } while(Next(4));
     return result;
 }
 
