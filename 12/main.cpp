4bad9ad83a7b7e156723e4303c19294f1f4313d1
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-14 11:45:36 +0100

    TEST RUN: Thu Dec 14 11:45:36 AM CET 2023

--- 12/main.cpp
+++ 12/main.cpp
@@ -98,6 +98,7 @@ bool CheckFromHere(S& s, LL posS, LL num){
 LL recursion(const S& s, LL posS, const VECI& nums, LL posN){
     P_LINE;
     while(posS < s.size() && s[posS] == '.'){
+        P_LINE;
         ++posS;
     }
     LL num = nums[posN];
