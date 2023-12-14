9a8d672f2e9d688647ed954d57f2bbef3c579230
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-14 02:05:42 +0100

    TEST RUN: Thu Dec 14 02:05:42 AM CET 2023

--- 10/main.cpp
+++ 10/main.cpp
@@ -9,16 +9,24 @@
 
 auto in = getInput();
 
-LL Y(LL n){
-    return n/in[0].size();
+LL Y(LL pos){
+    return pos/in[0].size();
 }
 
-LL X(LL n){
-    return n%in[0].size();
+LL X(LL pos){
+    return pos%in[0].size();
 }
 
-char Get(LL n){
-    return in[Y(n)][X(n)];
+LL N(LL x, LL y){
+    return y*in[0].size() + x;
+}
+
+char Get(LL pos){
+    return in[Y(pos)][X(pos)];
+}
+
+void Set(LL pos, LL n){
+    in[Y(pos)][X(pos)] = n;
 }
 
 LL findStart(){
@@ -27,23 +35,94 @@ LL findStart(){
             return i;
         }
     }
+    P_LINE;
     exit(1);
 }
 
-auto count() {
+LL Up(LL n){
+    auto x = X(n);
+    auto y = Y(n);
+    return N(x, y+1);
+}
+
+LL Down(LL n){
+    auto x = X(n);
+    auto y = Y(n);
+    return N(x, y-1);
+}
+
+LL Left(LL n){
+    auto x = X(n);
+    auto y = Y(n);
+    return N(x-1, y);
+}
+
+LL Right(LL n){
+    auto x = X(n);
+    auto y = Y(n);
+    return N(x+1, y);
+}
+
+VECI GetConnections(LL n){
+    switch(Get(n)){
+        case '|':
+            return {Up(n), Down(n)};
+        case '-':
+            return {Left(n), Right(n)};
+        case 'L':
+            return {Up(n), Right(n)};
+        case 'J':
+            return {Up(n), Left(n)};
+        case '7':
+            return {Left(n), Down(n)};
+        case 'F':
+            return {Right(n), Down(n)};
+    }
+    P_LINE;
+    exit(1);
+}
+
+void GetNext(LL& pos, LL n){
+    LL nextPos;
+    Set(pos, n);
+    pos = nextPos;
+}
+
+auto count1() {
     LL result = 0;
-    result = findStart();
+    auto start = findStart();
+    auto l = Left(start);
+    auto r = Left(start);
+    auto u = Left(start);
+    auto d = Left(start);
+    auto conl = GetConnections(l);
+    auto conr = GetConnections(r);
+    auto conu = GetConnections(u);
+    auto cond = GetConnections(d);
+    VECI conStart;
+    if(conl[0] == start || conl[1] == start) conStart.push_back(l);
+    if(conr[0] == start || conr[1] == start) conStart.push_back(r);
+    if(conu[0] == start || conu[1] == start) conStart.push_back(u);
+    if(cond[0] == start || cond[1] == start) conStart.push_back(d);
+
+    if(conStart.size() != 2){P_LINE; exit(1);}
+
+    auto p1 = conStart[0];
+    auto p2 = conStart[1];
+
+    P(start, p1, p2);
+
     return result;
 }
 
 int main(int argc, char** argv)
 {
     LL score = 0;
-    score = count();
+    score = count1();
     P_RR("Part1: %lld\n", score);
     //========================================================
 
-    score = count();
+    score = count1();
     P_RR("Part2: %lld\n", score);
     return 0;
 }
