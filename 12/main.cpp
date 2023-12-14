2eccffd44af881e091a2bf23e0fee6538cdbd632
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-14 11:58:29 +0100

    TEST RUN: Thu Dec 14 11:58:29 AM CET 2023

--- 12/main.cpp
+++ 12/main.cpp
@@ -101,39 +101,39 @@ bool CheckFromHere(S& s, LL posS, LL num){
 }
 
 LL recursion(const S& s, LL posS, const VECI& nums, LL posN){
-        P(s, posS, s[posS], nums.size(), posN);
+        // P(s, posS, s[posS], nums.size(), posN);
     while(posS < s.size() && s[posS] == '.'){
-        P_LINE;
+        // P_LINE;
         ++posS;
     }
     if(s.size() <= posS) {
-        P_LINE;
+        // P_LINE;
         if(nums.size() == posN) {
             P(s);
             return 1;
         }
-        P_LINE;
+        // P_LINE;
         return 0;
     } else if(s[posS] == '#'){
         if(posN == nums.size()) {
             return 0;
         }
-        P_LINE;
+        // P_LINE;
         S ss = s;
         LL num = nums[posN];
         if(!CheckFromHere(ss, posS, num)) {
-            P_LINE;
+            // P_LINE;
             return 0;
         }
         return recursion(ss, posS+num+1, nums, posN+1);
     } else if(s[posS] == '?') {
-        P_LINE;
+        // P_LINE;
         LL total = 0;
         S ss = s;
         if(posN < nums.size()) {
             LL num = nums[posN];
             if(CheckFromHere(ss, posS, num)) {
-                P_LINE;
+                // P_LINE;
                 total += recursion(ss, posS+num+1, nums, posN+1);
             }
         }
