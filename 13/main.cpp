460d78726877560412b278ce89b0b00956da61c5
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-12-14 16:44:33 +0100

    REAL RUN: Thu Dec 14 04:44:33 PM CET 2023

--- 13/main.cpp
+++ 13/main.cpp
@@ -9,8 +9,42 @@
 
 auto in = getInput();
 
+LL FindRefl(const VECS& pattern){
+    for(size_t i = 1; i < pattern.size(); ++i){
+        if(pattern[i-1] == pattern[i]) {
+            return i-1;
+        }
+    }
+    for(size_t i = 1; i < pattern[0].size(); ++i){
+        bool find = true;
+        FOR(j, pattern.size()){
+            if(pattern[j][i-1] != pattern[j][i]){
+                find = false;
+                break;
+            }
+        }
+        if(find){
+            return i-1;
+        }
+    }
+    P_LINE;
+    exit(1);
+}
+
 auto count() {
     LL result = 0;
+
+    VECS pattern;
+    for(const auto& s:in){
+        if(!s.empty()){
+            pattern.push_back(s);
+        } else {
+            auto local = FindRefl(pattern);
+            pattern.clear();
+            result += local;
+        }
+    }
+
     return result;
 }
 
