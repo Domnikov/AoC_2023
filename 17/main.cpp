a72fa6af19a10fb89d498f0dc99f8a6489cfc3b3
ivan domnikov <ivan@haleytek.com>
2023-12-19 22:04:46 +0100

    TEST RUN: tis 19 dec 2023 22:04:46 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -99,13 +99,13 @@ auto count1() {
     // P_VECV(in);
     // P_RR("\n");
 
-    for(LL i = 0;!points.empty() && i < 1000;++i){
+    for(LL i = 0;!points.empty() && i < 10000;++i){
         auto [pos, score, d, path] = ExtractMinPos(points);
         if(pos == endPos) {
             return score;
         }
         AddNewPos(pos, score, d, path, points);
-        P(pos, score, points);
+        // P(pos, score, points);
         // P_VECV(in);
         // P_RR("\n");
     }
