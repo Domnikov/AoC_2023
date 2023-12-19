3fc1d93f1f0e6edee9f0bc2921af613d1a9cb6c7
ivan domnikov <ivan@haleytek.com>
2023-12-19 13:38:14 +0100

    TEST RUN: tis 19 dec 2023 13:38:14 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -9,6 +9,65 @@
 
 auto in = getInput();
 
+LL X = in[0].size();
+LL Y = in.size();
+
+enum class Dir {
+    None  = 0,
+    Left  = 0x1,
+    Right = 0x10,
+    Up    = 0x100,
+    Down  = 0x1000,
+};
+
+namespace std{
+
+    inline ostream& operator<<( ostream& dest, Dir d )
+    {
+        switch (d) {
+            case Dir::None : dest << "None "; break;
+            case Dir::Left : dest << "Left "; break;
+            case Dir::Right: dest << "Right"; break;
+            case Dir::Up   : dest << "Up   "; break;
+            case Dir::Down : dest << "Down "; break;
+        }
+        return dest;
+    }
+}
+
+LL GetX(LL pos) {
+    return pos % X;
+}
+
+LL GetY(LL pos) {
+    return pos / X;
+}
+
+LL GetPos(LL x, LL y) {
+    return y*X + x;
+}
+
+LL ToLeft (LL pos) { LL x = GetX(pos); LL y = GetY(pos); if( x == 0) { throw S("x < 0"); } return GetPos(x-1, y  ); }
+LL ToRight(LL pos) { LL x = GetX(pos); LL y = GetY(pos); if( x>=X-1) { throw S("x > X"); } return GetPos(x+1, y  ); }
+LL ToUp   (LL pos) { LL x = GetX(pos); LL y = GetY(pos); if( y == 0) { throw S("y < 0"); } return GetPos(x  , y-1); }
+LL ToDown (LL pos) { LL x = GetX(pos); LL y = GetY(pos); if( y>=Y-1) { throw S("y > Y"); } return GetPos(x  , y+1); }
+
+char GetC(LL pos){
+    return in[GetY(pos)][GetX(pos)];
+}
+
+LL Move(Dir d, LL pos) {
+    switch(d) {
+        case Dir::None : throw(S("Not a direction"));
+        case Dir::Left : return ToLeft (pos);
+        case Dir::Right: return ToRight(pos);
+        case Dir::Up   : return ToUp   (pos);
+        case Dir::Down : return ToDown (pos);
+    }
+    P_LINE;
+    exit(1);
+}
+
 auto count1() {
     LL result = 0;
 
