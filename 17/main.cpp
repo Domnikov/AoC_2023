e7da57297ce08e92c288b2884522e65574493ff2
ivan domnikov <ivan@haleytek.com>
2023-12-20 13:15:20 +0100

    TEST RUN: ons 20 dec 2023 13:15:20 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -9,7 +9,7 @@
 
 auto in = getInput();
 
-constexpr bool USE_PATH = true;
+constexpr bool USE_PATH = false;
 
 LL X = in[0].size();
 LL Y = in.size();
@@ -167,7 +167,7 @@ auto count1() {
     // P_VECV(in);
     // P_RR("\n");
     LL counter = 100;
-    for(LL i = 0;!points.empty() && i < 10000;++i){
+    for(LL i = 0;!points.empty() /*&& i < 10000*/;++i){
         const auto& path = ExtractMinPos(points);
         auto [pos, score, d, dir_counter, path_vec] = path;
         if(pos == endPos) {
