b819e606c8fce74b8a2012afbb82ab828189b747
ivan domnikov <ivan@haleytek.com>
2023-12-20 13:13:32 +0100

    TEST RUN: ons 20 dec 2023 13:13:32 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -214,10 +214,7 @@ auto count1() {
             // for(auto p: path){
             //     SetC(p, '*', input_copy);
             // }
-            // P_VECV(input_copy);
-        for(const auto& s : in) {
-            P_RR("%s\n", s.c_str());
-        }
+            P_VECV(in);
         P_LINE;
             for(const auto& p:points) {
                 P(p);
