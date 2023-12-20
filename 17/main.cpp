34993ead49207d5f5da596b065006ffd4ce0204e
ivan domnikov <ivan@haleytek.com>
2023-12-20 11:04:57 +0100

    TEST RUN: ons 20 dec 2023 11:04:57 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -116,15 +116,15 @@ auto count1() {
     LL counter = 1;
     for(LL i = 0;!points.empty() /*&& i < 1000000000*/;++i){
         const auto& path = ExtractMinPos(points);
-        auto [pos, score, d, dir_counter, vec] = path;
+        auto [pos, score, d, dir_counter, path_vec] = path;
         if(pos == endPos) {
-            // in = getInput();
-            // LL sc = 0;
-            // for(auto p: path){
-            //     sc += GetScore(p);
-            //     SetC(p, '*');
-            // }
-            // P(sc);
+            in = getInput();
+            LL sc = 0;
+            for(auto p: path_vec){
+                sc += GetScore(p);
+                SetC(p, '*');
+            }
+            P(sc);
             P_VECV(in);
             P_RR("\n");
             // Dir d1 = std::get<2>(path[path.size() - 1]);
