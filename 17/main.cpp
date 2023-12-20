be73073313d85d2e215102345f8f0d5e8b64d3e3
ivan domnikov <ivan@haleytek.com>
2023-12-20 21:47:35 +0100

    TEST RUN: ons 20 dec 2023 21:47:35 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -115,11 +115,15 @@ auto count1() {
     points.emplace_back(0, GetScore(0), Dir::Up, 0, VECI{0});
     LL counter = 100;
     for(LL i = 0;!points.empty() && i < 10;++i){
+        P(i);
         auto [pos, score, dir, dir_count, path] = GetMin();
         if(pos == endPos) {
             return score;
         }
         AddNew(pos, score, dir, dir_count, path);
+        for(const auto& p:points) {
+            P(p);
+        }
     }
 
     for(const auto& p:points) {
