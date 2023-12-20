89a9e3091faee23489a01bea89ef1f841accbb4b
ivan domnikov <ivan@haleytek.com>
2023-12-20 13:07:42 +0100

    TEST RUN: ons 20 dec 2023 13:07:42 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -97,6 +97,7 @@ auto ExtractMinPos(std::list<Path>& vec) {
     auto result = *it;
     // vec.erase(it);
     if (isFool(std::get<0>(result))) {
+        P_LINE;
         SetC(std::get<0>(result), 'X');
         vec.erase(it);
     }
