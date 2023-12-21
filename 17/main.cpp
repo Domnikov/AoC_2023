f02a592c91e7f62e088064b96da7fc9e88cf4ee4
ivan domnikov <ivan@haleytek.com>
2023-12-21 20:56:11 +0100

    TEST RUN: tor 21 dec 2023 20:56:11 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -85,79 +85,6 @@ LL GetScore(LL pos){
     return -1;
 }
 
-#if 0
-std::list<Path> points;
-std::map<LL, LL> passed;
-
-Path GetMin(){
-    auto it = std::min_element(BE(points), [](const auto& a, const auto& b){return (std::get<1>(a) < std::get<1>(b));});
-    assert(it != points.end());
-    auto path = *it;
-    points.erase(it);
-    passed.emplace(std::get<0>(path), std::get<1>(path));
-    return path;
-}
-
-void InsertIfLess(LL newPos, LL newScore, Dir dir, LL dir_count, const VECI& path) {
-    if(passed.count(newPos)){
-        if(passed[newPos] < newScore) {
-            return;
-        } else {
-            passed.erase(newPos);
-        }
-    }
-    auto it = std::find_if(BE(points), [newPos](const auto& a){return std::get<0>(a) == newPos;});
-    if(it == points.end()){
-        points.emplace_back(newPos, newScore, dir, dir_count, path);
-        SetC(newPos, '*', in2);
-    } else if(newScore < std::get<1>(*it)) {
-        *it = Path{newPos, newScore, dir, dir_count, path};
-    }
-}
-
-void InsertIfCorrect(LL newPos, LL newScore, Dir dir, LL dir_count, VECI& path) {
-    if(path.size() >= 3) {
-        LL x5 = GetX(path[path.size()-5]);
-        LL x4 = GetX(path[path.size()-4]);
-        LL x3 = GetX(path[path.size()-3]);
-        LL x2 = GetX(path[path.size()-2]);
-        LL x1 = GetX(path[path.size()-1]);
-
-        LL y5 = GetY(path[path.size()-5]);
-        LL y4 = GetY(path[path.size()-4]);
-        LL y3 = GetY(path[path.size()-3]);
-        LL y2 = GetY(path[path.size()-2]);
-        LL y1 = GetY(path[path.size()-1]);
-
-        if((x1 == x2 && x2 == x3 && x3 == x4 && x4 == x5) || (y1 == y2 && y2 == y3 && y3 == y4 && y4 == y5)) {
-            return;
-        }
-    }
-
-    if(std::find(path.begin(), path.end(), newPos) == path.end()) {
-        if(USE_PATH) {
-            path.push_back(newPos);
-        }
-        points.emplace_back(newPos, newScore, dir, dir_count, path);
-    }
-}
-
-void AddNew(LL pos, LL score, Dir dir, LL dir_count, const VECI& path) {
-    if(dir != Dir::Right){LL newPos = pos; LL newScore = score; VECI newPath; if(USE_PATH){newPath.reserve(path.size()+1); newPath = path;} LL newDirCount =  0; for(LL i = 0;i< 1;++i){newPos = ToLeft (newPos); if(newPos == -1){break;} LL sc = GetScore(newPos); if(sc == -1){break;} newScore += sc; InsertIfCorrect(newPos, newScore, Dir::Left , newDirCount+i+1, newPath);}}
-    if(dir != Dir::Left ){LL newPos = pos; LL newScore = score; VECI newPath; if(USE_PATH){newPath.reserve(path.size()+1); newPath = path;} LL newDirCount =  0; for(LL i = 0;i< 1;++i){newPos = ToRight(newPos); if(newPos == -1){break;} LL sc = GetScore(newPos); if(sc == -1){break;} newScore += sc; InsertIfCorrect(newPos, newScore, Dir::Right, newDirCount+i+1, newPath);}}
-    if(dir != Dir::Down ){LL newPos = pos; LL newScore = score; VECI newPath; if(USE_PATH){newPath.reserve(path.size()+1); newPath = path;} LL newDirCount =  0; for(LL i = 0;i< 1;++i){newPos = ToUp   (newPos); if(newPos == -1){break;} LL sc = GetScore(newPos); if(sc == -1){break;} newScore += sc; InsertIfCorrect(newPos, newScore, Dir::Up   , newDirCount+i+1, newPath);}}
-    if(dir != Dir::Up   ){LL newPos = pos; LL newScore = score; VECI newPath; if(USE_PATH){newPath.reserve(path.size()+1); newPath = path;} LL newDirCount =  0; for(LL i = 0;i< 1;++i){newPos = ToDown (newPos); if(newPos == -1){break;} LL sc = GetScore(newPos); if(sc == -1){break;} newScore += sc; InsertIfCorrect(newPos, newScore, Dir::Down , newDirCount+i+1, newPath);}}
-}
-
-void AddNew3(LL pos, LL score, Dir dir, LL dir_count, const VECI& path) {
-    if(dir != Dir::Right){LL newPos = pos; LL newScore = score; VECI newPath; if(USE_PATH){newPath.reserve(path.size()+3); newPath = path;} LL newDirCount =  (dir != Dir::Left ) ? 0 : dir_count; for(LL i = 0;i< 3-newDirCount;++i){newPos = ToLeft (newPos); if(newPos == -1){break;} LL sc = GetScore(newPos); if(sc == -1){break;} newScore += sc; if(USE_PATH){newPath.push_back(newPos);}InsertIfLess(newPos, newScore, Dir::Left , newDirCount+i+1, newPath);}}
-    if(dir != Dir::Left ){LL newPos = pos; LL newScore = score; VECI newPath; if(USE_PATH){newPath.reserve(path.size()+3); newPath = path;} LL newDirCount =  (dir != Dir::Right) ? 0 : dir_count; for(LL i = 0;i< 3-newDirCount;++i){newPos = ToRight(newPos); if(newPos == -1){break;} LL sc = GetScore(newPos); if(sc == -1){break;} newScore += sc; if(USE_PATH){newPath.push_back(newPos);}InsertIfLess(newPos, newScore, Dir::Right, newDirCount+i+1, newPath);}}
-    if(dir != Dir::Down ){LL newPos = pos; LL newScore = score; VECI newPath; if(USE_PATH){newPath.reserve(path.size()+3); newPath = path;} LL newDirCount =  (dir != Dir::Up   ) ? 0 : dir_count; for(LL i = 0;i< 3-newDirCount;++i){newPos = ToUp   (newPos); if(newPos == -1){break;} LL sc = GetScore(newPos); if(sc == -1){break;} newScore += sc; if(USE_PATH){newPath.push_back(newPos);}InsertIfLess(newPos, newScore, Dir::Up   , newDirCount+i+1, newPath);}}
-    if(dir != Dir::Up   ){LL newPos = pos; LL newScore = score; VECI newPath; if(USE_PATH){newPath.reserve(path.size()+3); newPath = path;} LL newDirCount =  (dir != Dir::Down ) ? 0 : dir_count; for(LL i = 0;i< 3-newDirCount;++i){newPos = ToDown (newPos); if(newPos == -1){break;} LL sc = GetScore(newPos); if(sc == -1){break;} newScore += sc; if(USE_PATH){newPath.push_back(newPos);}InsertIfLess(newPos, newScore, Dir::Down , newDirCount+i+1, newPath);}}
-}
-
-#endif
-
 struct Q {
     Q(LL p, LL s, Dir d, LL dc, VECI path = {})
         :pos(p),score(s),dir(d),dir_count(dc), path(path)
@@ -202,17 +129,14 @@ void CheckAndInsert(LL newPos, LL score, Dir dir, LL dir_count, const VECI& path
     VECI newPath;
     if(USE_PATH) {newPath.reserve(path.size()+1); newPath = path; newPath.push_back(newPos);}
     auto& oldScore = matrix[newPos][newDir][newDirCount];
-    // P(newPos, newScore, newDir, newDirCount, newPath, newDir, oldScore);
-    if(newPos == GetPos(X-1, Y-1)) {
-        auto newIn = getInput();
-        for(auto p:newPath){
-            SetC(p,'*', newIn);
-        }
-        P_VECV(newIn);
-        P(newScore);
-        // P_VECV(in);
-        // return score;
-    }
+    // if(newPos == GetPos(X-1, Y-1)) {
+    //     auto newIn = getInput();
+    //     for(auto p:newPath){
+    //         SetC(p,'*', newIn);
+    //     }
+    //     P_VECV(newIn);
+    //     P(newScore);
+    // }
     if(oldScore > newScore) {
         oldScore = newScore;
         queue.emplace_back(newPos, newScore, newDir, newDirCount, newPath);
@@ -223,65 +147,21 @@ void CheckAndInsert(LL newPos, LL score, Dir dir, LL dir_count, const VECI& path
 void AddLeft (LL pos, LL score, Dir dir, LL dir_count, const VECI& path){
     LL newPos = ToLeft(pos);
     CheckAndInsert(newPos, score, dir, dir_count, path, Dir::Left);
-    // if(newPos == -1) return;
-    // score += GetScore(newPos);
-    // VECI newPath;
-    // if(USE_PATH) {newPath.reserve(path.size()+2); newPath = path; newPath.push_back(newPos);}
-    // newPos = ToLeft(newPos);
-    // CheckAndInsert(newPos, score, dir, dir_count+1, newPath, Dir::Left);
-    // if(newPos == -1) return;
-    // score += GetScore(newPos);
-    // if(USE_PATH)newPath.push_back(newPos);
-    // newPos = ToLeft(newPos);
-    // CheckAndInsert(newPos, score, dir, dir_count+2, newPath, Dir::Left);
 }
 
 void AddRight(LL pos, LL score, Dir dir, LL dir_count, const VECI& path){
     LL newPos = ToRight(pos);
     CheckAndInsert(newPos, score, dir, dir_count, path, Dir::Right);
-    // if(newPos == -1) return;
-    // score += GetScore(newPos);
-    // VECI newPath;
-    // if(USE_PATH) {newPath.reserve(path.size()+2); newPath = path; newPath.push_back(newPos);}
-    // newPos = ToRight(newPos);
-    // CheckAndInsert(newPos, score, dir, dir_count+1, newPath, Dir::Right);
-    // if(newPos == -1) return;
-    // score += GetScore(newPos);
-    // if(USE_PATH)newPath.push_back(newPos);
-    // newPos = ToRight(newPos);
-    // CheckAndInsert(newPos, score, dir, dir_count+2, newPath, Dir::Right);
 }
 
 void AddUp   (LL pos, LL score, Dir dir, LL dir_count, const VECI& path){
     LL newPos = ToUp(pos);
     CheckAndInsert(newPos, score, dir, dir_count, path, Dir::Up);
-    // if(newPos == -1) return;
-    // score += GetScore(newPos);
-    // VECI newPath;
-    // if(USE_PATH) {newPath.reserve(path.size()+2); newPath = path; newPath.push_back(newPos);}
-    // newPos = ToUp(newPos);
-    // CheckAndInsert(newPos, score, dir, dir_count+1, newPath, Dir::Up);
-    // if(newPos == -1) return;
-    // score += GetScore(newPos);
-    // if(USE_PATH)newPath.push_back(newPos);
-    // newPos = ToUp(newPos);
-    // CheckAndInsert(newPos, score, dir, dir_count+2, newPath, Dir::Up);
 }
 
 void AddDown (LL pos, LL score, Dir dir, LL dir_count, const VECI& path){
     LL newPos = ToDown(pos);
     CheckAndInsert(newPos, score, dir, dir_count, path, Dir::Down);
-    // if(newPos == -1) return;
-    // score += GetScore(newPos);
-    // VECI newPath;
-    // if(USE_PATH) {newPath.reserve(path.size()+2); newPath = path; newPath.push_back(newPos);}
-    // newPos = ToDown(newPos);
-    // CheckAndInsert(newPos, score, dir, dir_count+1, newPath, Dir::Down);
-    // if(newPos == -1) return;
-    // score += GetScore(newPos);
-    // if(USE_PATH)newPath.push_back(newPos);
-    // newPos = ToDown(newPos);
-    // CheckAndInsert(newPos, score, dir, dir_count+2, newPath, Dir::Down);
 }
 
 
@@ -293,7 +173,6 @@ void AddNew(LL pos, LL score, Dir dir, LL dir_count, const VECI& path) {
 }
 
 auto count1() {
-    LL result = 0;
     LL endPos = GetPos(X-1, Y-1);
 
     FOR(i, X*Y){
@@ -305,31 +184,20 @@ auto count1() {
         matrix.push_back(map);
     }
 
-    queue.emplace_back(0, /*GetScore(0)*/0, Dir::None, 0, VECI{0});
+    queue.emplace_back(0, 0, Dir::None, 0, VECI{0});
 
-    for(LL i = 0;!queue.empty()/* && i < 1000000*/;++i){
-        // P(i);
+    for(LL i = 0;!queue.empty();++i){
         auto [pos, score, dir, dir_count, path] = GetMin();
         SetC(pos, '#', in2);
         AddNew(pos, score, dir, dir_count, path);
-        // P(i, pos, score, dir, dir_count, path)
-        // for(const auto& p:queue) {
-        //     P(p);
-        // }
     }
 
-    P("Not found!!!");
-    // for(auto pnt:points){
-    //     LL p = std::get<0>(pnt);
-    //     SetC(p,'*', newIn);
-    // }
-    // P_VECV(in2);
-    for(const auto& p:queue) {
-        P(p);
+    LL result = 9999;
+    for(auto m:matrix[endPos]){
+        for(auto v: m.second){
+            if(v<result) result = v;
+        }
     }
-    // for(const auto& m:matrix[endPos]) {
-    // }
-    P_MAP(matrix[2]);
     P_MAP(matrix[endPos]);
 
     return result;
