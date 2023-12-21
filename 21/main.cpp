1c3dc333d7f4bb0eed61914c55e3f12ea6c48b59
ivan domnikov <ivan@haleytek.com>
2023-12-21 23:46:32 +0100

    Add other days

---
+++ 21/main.cpp
@@ -0,0 +1,34 @@
+#ifdef TEST
+#include "in_test.hpp"
+#else
+#include "in.hpp"
+#endif
+
+#include <queue>
+#include <list>
+
+auto in = getInput();
+
+auto count1() {
+    LL result = 0;
+
+    return result;
+}
+
+auto count2() {
+    LL result = 0;
+
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    LL score = 0;
+    score = count1();
+    P_RR("Part1: %lld\n", score);
+    //========================================================
+
+    score = count2();
+    P_RR("Part2: %lld\n", score);
+    return 0;
+}
