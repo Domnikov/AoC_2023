0eaf8a3a0ff311a9938da6eee8c468042382a952
ivan domnikov <ivan@haleytek.com>
2023-12-21 20:38:35 +0100

    TEST RUN: tor 21 dec 2023 20:38:35 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -202,7 +202,7 @@ void CheckAndInsert(LL newPos, LL score, Dir dir, LL dir_count, const VECI& path
     VECI newPath;
     if(USE_PATH) {newPath.reserve(path.size()+1); newPath = path; newPath.push_back(newPos);}
     P(newPos, newDir, dir_count);
-    auto& oldScore = matrix[newPos][newDir][dir_count];
+    auto& oldScore = matrix[newPos][newDir][newDirCount];
     // P(newPos, newScore, newDir, newDirCount, newPath, newDir, oldScore);
     if(newPos == 2){//GetPos(X-1, Y-1)) {
         auto newIn = getInput();
