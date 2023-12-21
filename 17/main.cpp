78da423e0b947937f352b59f3749fcdfb81cecb5
ivan domnikov <ivan@haleytek.com>
2023-12-21 20:29:03 +0100

    TEST RUN: tor 21 dec 2023 20:29:03 CET

--- 17/main.cpp
+++ 17/main.cpp
@@ -223,6 +223,10 @@ void CheckAndInsert(LL newPos, LL score, Dir dir, LL dir_count, const VECI& path
 void AddLeft (LL pos, LL score, Dir dir, LL dir_count, const VECI& path){
     LL newPos = ToLeft(pos);
     CheckAndInsert(newPos, score, dir, dir_count, path, Dir::Left);
+    newPos = ToLeft(newPos);
+    CheckAndInsert(newPos, score, dir, dir_count+1, path, Dir::Left);
+    newPos = ToLeft(newPos);
+    CheckAndInsert(newPos, score, dir, dir_count+2, path, Dir::Left);
 }
 
 void AddRight(LL pos, LL score, Dir dir, LL dir_count, const VECI& path){
