ab89bad901a3010ca8345c7c7f592a6557616444
ivan domnikov <ivan@haleytek.com>
2023-12-22 15:16:41 +0100

    TEST RUN: fre 22 dec 2023 15:16:41 CET

--- 20/main.cpp
+++ 20/main.cpp
@@ -36,6 +36,7 @@ int main(int argc, char** argv)
         } else if(type == '&') {
             cjmod[name] = outs;
         } else {
+            P(type);
             P_LINE;
             exit(1);
         }
