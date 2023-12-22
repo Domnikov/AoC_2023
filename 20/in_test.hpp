12c87bf462220599b46ff1366acc0374633ba2e5
ivan domnikov <ivan@haleytek.com>
2023-12-22 15:16:27 +0100

    TEST RUN: fre 22 dec 2023 15:16:27 CET

--- 20/in_test.hpp
+++ 20/in_test.hpp
@@ -4,6 +4,11 @@
 inline auto getInput()
 {
     VECS input {
+"broadcaster -> a, b, c",
+"%a -> b",
+"%b -> c",
+"%c -> inv",
+"&inv -> a",
     };
 
     return input;
--- 20/main.cpp
+++ 20/main.cpp
@@ -8,6 +8,8 @@
 #include <list>
 
 auto in = getInput();
+std::map<S, VECS> ffmod;
+std::map<S, VECS> cjmod;
 
 auto count1() {
     LL result = 0;
@@ -23,6 +25,26 @@ auto count2() {
 
 int main(int argc, char** argv)
 {
+    for(const auto& l: in){
+        auto v = splitStr(l, '>');
+        auto name = splitStr(v[0], ' ')[0];
+        auto outs = splitStr(v[1], ',');
+        char type = name[0];
+        name.erase(0,1);
+        if(type == '%') {
+            ffmod[name] = outs;
+        } else if(type == '&') {
+            cjmod[name] = outs;
+        } else {
+            P_LINE;
+            exit(1);
+        }
+    }
+
+    P_MAP(ffmod);
+    P_MAP(cjmod);
+
+
     LL score = 0;
     score = count1();
     P_RR("Part1: %lld\n", score);
