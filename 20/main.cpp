ff69442e76ad2deae2177ff061341aed9c33ad72
ivan domnikov <ivan@haleytek.com>
2023-12-27 00:32:26 +0100

    REAL RUN: ons 27 dec 2023 00:32:26 CET

--- 20/main.cpp
+++ 20/main.cpp
@@ -214,7 +214,7 @@ int main(int argc, char** argv)
     }
 
     LL score = 0;
-    score = count1();
+    // score = count1();
     P_RR("Part1: %lld\n", score);
     //========================================================
 
