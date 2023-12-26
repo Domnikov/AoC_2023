e089790ce4bb25bfe082253acfe887c196c6c1a9
ivan domnikov <ivan@haleytek.com>
2023-12-26 23:43:26 +0100

    TEST RUN: tis 26 dec 2023 23:43:26 CET

--- 20/in_test.hpp
+++ 20/in_test.hpp
@@ -4,17 +4,17 @@
 inline auto getInput()
 {
     VECS input {
-"broadcaster -> a, b, c",
-"%a -> b",
-"%b -> c",
-"%c -> inv",
-"&inv -> a",
+// "broadcaster -> a, b, c",
+// "%a -> b",
+// "%b -> c",
+// "%c -> inv",
+// "&inv -> a",
 
-// "broadcaster -> a",
-// "%a -> inv, con",
-// "&inv -> b",
-// "%b -> con",
-// "&con -> output",
+"broadcaster -> a",
+"%a -> inv, con",
+"&inv -> b",
+"%b -> con",
+"&con -> output",
     };
 
     return input;
