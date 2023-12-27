fea7997a4be2c29ec907c6d97e0b6a46e440ef8c
ivan domnikov <ivan@haleytek.com>
2023-12-27 12:33:14 +0100

    REAL RUN: ons 27 dec 2023 12:33:14 CET

--- 21/main.cpp
+++ 21/main.cpp
@@ -27,16 +27,10 @@ std::ostream& operator<<(std::ostream& os, const Elf& me) {
     return os;
 }
 
-auto count1() {
-    LL result = 0;
-
-    return result;
-}
-
-auto count2() {
-    LL result = 0;
-
-    return result;
+bool operator<(const Elf& lhs, const Elf& rhs) {
+    LL s1 = lhs.row*1000+lhs.col;
+    LL s2 = rhs.row*1000+rhs.col;
+    return s1 < s2;
 }
 
 Elf GetFirst(){
@@ -50,10 +44,40 @@ Elf GetFirst(){
     exit(1);
 }
 
+void step(std::set<Elf>& elfs){
+    std::set<Elf> copy;
+    for(auto& org:elfs){
+        if(in[org.row - 1][org.col    ] == '.') {copy.emplace(org.row - 1, org.col    );}
+        if(in[org.row + 1][org.col    ] == '.') {copy.emplace(org.row + 1, org.col    );}
+        if(in[org.row    ][org.col - 1] == '.') {copy.emplace(org.row    , org.col - 1);}
+        if(in[org.row    ][org.col + 1] == '.') {copy.emplace(org.row    , org.col + 1);}
+    }
+    elfs.swap(copy);
+}
+
+auto count1() {
+    LL result = 0;
+    std::set<Elf> elfs{GetFirst()};
+
+    LL N = 1;
+    FOR(i, N){
+        step(elfs);
+    }
+    for(const auto& e:elfs) {
+        P(e);
+    }
+
+    return result;
+}
+
+auto count2() {
+    LL result = 0;
+
+    return result;
+}
+
 int main(int argc, char** argv)
 {
-    auto elf = GetFirst();
-    P(elf);
     LL score = 0;
     score = count1();
     P_RR("Part1: %lld\n", score);
