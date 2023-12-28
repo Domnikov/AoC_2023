c0b1c4f498c4e1ef0087d8cc55dfcd31fcc9c3ab
ivan domnikov <ivan@haleytek.com>
2023-12-29 00:18:58 +0100

    TEST RUN: fre 29 dec 2023 00:18:58 CET

--- 22/main.cpp
+++ 22/main.cpp
@@ -52,10 +52,48 @@ std::vector<Brik> GetAllBriks(){
     return briks;
 }
 
+bool MoveDownOne(std::set<LL>& used, Brik& brik){
+    LL x1 = brik.x1;
+    LL x2 = brik.x2;
+    LL y1 = brik.y1;
+    LL y2 = brik.y2;
+    LL z1 = brik.z1-1;
+    LL z2 = brik.z2-1;
+    for(LL x = brik.x1; x <= brik.x2;x++)
+        for(LL y = brik.y1; y <= brik.y2;y++)
+            for(LL z = brik.z1; z <= brik.z2;z++){
+                if(used.count(x*1000000+y*1000+z)) {
+                    return false;
+                }
+            }
+    brik.z1 = z1;
+    brik.z2 = z2;
+    return true;
+}
+
+void Fix(std::set<LL>& used, const Brik& brik){
+    for(LL x = brik.x1; x <= brik.x2;x++)
+        for(LL y = brik.y1; y <= brik.y2;y++)
+            for(LL z = brik.z1; z <= brik.z2;z++){
+                used.insert(x*1000000+y*1000+z);
+            }
+}
+
+void MoveDownAll(std::vector<Brik>& briks) {
+    std::set<LL> used;
+    for(auto& b:briks){
+        while(MoveDownOne(used, b)){}
+        Fix(used, b);
+    }
+
+}
+
 auto count1() {
     LL result = 0;
     auto briks = GetAllBriks();
     std::sort(BE(briks));
+    P_VECV(briks);
+    MoveDownAll(briks);
     P_VECV(briks);
     return result;
 }
