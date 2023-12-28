8f8525e8b917f872352ed63c3861b38ecf9dd8e1
ivan domnikov <ivan@haleytek.com>
2023-12-28 15:27:17 +0100

    TEST RUN: tor 28 dec 2023 15:27:17 CET

--- 21/main.cpp
+++ 21/main.cpp
@@ -165,7 +165,7 @@ auto count2() {
             cache[elf] = Generate(elf);
         }
         auto& local = cache[elf];
-        // P(stp, X, Y, GetElfs(stp, local));
+        P(stp, X, Y, GetElfs(stp, local));
         q.pop();
         result += GetElfs(stp, local);
         FOR(i, 4) {
