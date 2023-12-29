ce9d643f2396f890942148e0490dce21a96b961f
ivan domnikov <ivan@haleytek.com>
2023-12-29 01:39:26 +0100

    TEST RUN: fre 29 dec 2023 01:39:26 CET

--- 23/main.cpp
+++ 23/main.cpp
@@ -91,7 +91,7 @@ auto count2() {
             // }
             const auto& m = Mods[i];
             auto newPos = cur+m;
-            P(newPos);
+            P(newPos, p.path);
             if(in[newPos.first][newPos.second] == '#') {
                 continue;
             }
