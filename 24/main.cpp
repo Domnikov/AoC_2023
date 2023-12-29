2710fdaf5522277d3a041ab20d34121d2a1627aa
ivan domnikov <ivan@haleytek.com>
2023-12-29 15:25:34 +0100

    TEST RUN: fre 29 dec 2023 15:25:34 CET

--- 24/main.cpp
+++ 24/main.cpp
@@ -89,7 +89,7 @@ auto count1() {
 
     for(const auto& pair : crossingLines) {
         auto [x,y] = pair.first.GetCP(pair.second);
-        P(x, y);
+        // P(x, y);
         if( L <= x && x <= H && L <= y && y <= H ) {
             result ++;
         }
