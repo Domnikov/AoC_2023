c75688a305b6a2dab36c6ab896411420256c2a60
ivan domnikov <ivan@haleytek.com>
2023-12-29 14:35:35 +0100

    TEST RUN: fre 29 dec 2023 14:35:35 CET

--- 24/main.cpp
+++ 24/main.cpp
@@ -20,7 +20,29 @@ struct Line2d{
         assert(vecp.size() == 3);
         assert(vecv.size() == 3);
 
+        double x1=vecp[0];
+        double y1=vecp[1];
 
+        double x2=x1+vecv[0];
+        double y2=y1+vecv[1];
+
+        a = y1 - y2;
+        b = x2 - x1;
+        c = x1*y2 - x2*y1;
+
+    }
+
+    bool IsParal(const Line2d& other) {
+        return ((a / other.a) == (b / other.b));
+    }
+
+    std::pair<double,double> intersect(const Line2d& other) {
+        double x, y;
+        double det = a * other.b - other.a * b;
+        x = (b * other.c - other.b * c) / det;
+        y = (other.a * c - a * other.c) / det;
+
+        return {x,y};
     }
 
     double a;
@@ -29,11 +51,21 @@ struct Line2d{
 };
 
 auto count1() {
+    LL result = 0;
     std::vector<Line2d> lines;
     std::transform(BE(in), std::back_inserter(lines), [](const auto& s){return Line2d(s);});
-    LL result = 0;
 
-    return result;
+    std::vector<std::pair<Line2d, Line2d>> crossingLines;
+
+    FOR(i, lines.size()){
+        for(LL j = i+1; j < lines.size();++j) {
+            if(lines[i].IsParal(lines[j]) == false) {
+                crossingLines.emplace_back(lines[i], lines[j]);
+            }
+        }
+    }
+
+    return result = crossingLines.size();
 }
 
 auto count2() {
