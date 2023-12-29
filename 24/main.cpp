bb4b936fa6d76fe538ea6515af54cd3b22daba9e
ivan domnikov <ivan@haleytek.com>
2023-12-29 14:23:55 +0100

    TEST RUN: fre 29 dec 2023 14:23:55 CET

--- 24/main.cpp
+++ 24/main.cpp
@@ -4,12 +4,33 @@
 #include "in.hpp"
 #endif
 
+#include <cassert>
 #include <queue>
 #include <list>
 
 auto in = getInput();
 
+struct Line2d{
+    Line2d(const S& str){
+        auto vecs = splitStr(str, '@');
+        assert(vecs.size() != 2);
+        auto vecp = vecsToVeci(splitStr(vecs[0], ','));
+        auto vecv = vecsToVeci(splitStr(vecs[1], ','));
+
+        assert(vecp.size() != 3);
+        assert(vecv.size() != 3);
+
+
+    }
+
+    double a;
+    double b;
+    double c;
+};
+
 auto count1() {
+    std::vector<Line2d> lines;
+    std::transform(BE(in), std::back_inserter(lines), [](const auto& s){return Line2d(s);});
     LL result = 0;
 
     return result;
