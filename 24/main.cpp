17ab4ac892a157dca00724ae7988938a68f6f37d
ivan domnikov <ivan@haleytek.com>
2023-12-30 00:42:34 +0100

    TEST RUN: lör 30 dec 2023 00:42:34 CET

--- 24/main.cpp
+++ 24/main.cpp
@@ -173,7 +173,7 @@ auto count2() {
                         LL Ck = lines[k].coord[n];
                         LL Vk = lines[k].velos[n];
                         auto cp = Line2d({(LL)C0, 0, 0}, {(LL)V0, 1, 0}).GetCP(Line2d{{Ck, 0, 0},{Vk, 1, 0}});
-                        if(cp.second == std::numeric_limits<long double>::infinity() || cp.second < 1) {
+                        if(cp.second == std::numeric_limits<long double>::quiet_NaN() || cp.second == std::numeric_limits<long double>::infinity() || cp.second < 1) {
                             found = false;
                             break;
                         }
