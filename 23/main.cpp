5f39e506f58cb6b0c28e789d2df7ad6542699942
ivan domnikov <ivan@haleytek.com>
2023-12-29 10:18:29 +0100

    TEST RUN: fre 29 dec 2023 10:18:29 CET

--- 23/main.cpp
+++ 23/main.cpp
@@ -86,9 +86,9 @@ bool TillNextCrossRoad(Path& path) {
                 continue;
             }
             if(std::find_if(BE(path.path), [newPos, ends = 0](const auto& p)mutable{
-            //                 if(p == std::make_pair(113, 125) || p == std::make_pair(137, 111)) {
+                            if(p == std::make_pair(113LL, 125LL) || p == std::make_pair(137LL, 111LL)) {
                                 ends++;
-            //                 }
+                            }
                             return ends == 2 || p == newPos;
                         }) != path.path.end()) {
                 continue;
