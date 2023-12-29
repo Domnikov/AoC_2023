27fddd284fe5a8da686930b55892af1898d49ec5
ivan domnikov <ivan@haleytek.com>
2023-12-29 23:54:54 +0100

    TEST RUN: fre 29 dec 2023 23:54:54 CET

--- 24/main.cpp
+++ 24/main.cpp
@@ -157,6 +157,8 @@ auto count2() {
                             found = false;
                             break;
                         }
+                        P(l.coord, l.velos, lines[k].coord, lines[k].velos);
+                        P(t, a, b, Ck, x1, C0, V0, Vk, locT);
                     }
                 }
                 if(found) {
