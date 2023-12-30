0108e0e20a2c6b1cd7a982ea7acf7b1690f11eec
ivan domnikov <ivan@haleytek.com>
2023-12-30 11:17:57 +0100

    TEST RUN: lör 30 dec 2023 11:17:57 CET

--- 25/main.cpp
+++ 25/main.cpp
@@ -9,6 +9,24 @@
 
 auto in = getInput();
 
+LL countConnected(const std::map<S,VECS>& map){
+    std::queue<S> q;
+    std::set<S> set;
+    q.push(map.begin()->first);
+    set.insert(map.begin()->first);
+    while(!q.empty()){
+        S name = q.front();
+        q.pop();
+        for(const auto& s:map.at(name)){
+            if(set.count(s) == 0){
+                q.push(s);
+                set.insert(s);
+            }
+        }
+    }
+    return set.size();
+}
+
 auto count1() {
     LL result = 0;
 
@@ -23,6 +41,8 @@ auto count1() {
         }
     }
     P_MAPV(map);
+
+    P(map.size(), countConnected(map));
 
     return result;
 }
