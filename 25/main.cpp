bc3a78976d870cb7a2d96d61f2141f52cb9efa7f
ivan domnikov <ivan@haleytek.com>
2023-12-30 12:44:04 +0100

    TEST RUN: lör 30 dec 2023 12:44:04 CET

--- 25/main.cpp
+++ 25/main.cpp
@@ -67,6 +67,7 @@ auto count1() {
         for(LL j = i+1; j < vec.size(); ++j){
             for(LL k = j+1; k < vec.size(); ++k){
                 result = std::min(result, countConnected(vec, i, j, k));
+                P(k);
             }
             P(j);
         }
